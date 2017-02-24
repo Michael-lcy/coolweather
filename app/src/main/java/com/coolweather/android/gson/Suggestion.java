523414ac203c6dd9e0dfd999dfa9b1a631e7ac65
Michael-lcy <516683850@qq.xom>
2017-02-24 16:00:40 +0800

    加入显示天气的功能

--- app/src/main/java/com/coolweather/android/gson/Suggestion.java
+++ app/src/main/java/com/coolweather/android/gson/Suggestion.java
@@ -7,27 +7,27 @@ import com.google.gson.annotations.SerializedName;
  */
 public class Suggestion {
 
-    @SerializedName("comf")
-    public Comfort comfort;
-
-    @SerializedName("cw")
-    public CarWash carwash;
-
-    public Suport suport;
-
-
-    private class Comfort {
-        @SerializedName("txt")
-        public String info;
-    }
-
-    private class CarWash {
-        @SerializedName("txt")
-        public String info;
-    }
-
-    private class Suport {
-        @SerializedName("txt")
-        public String info;
-    }
+//    @SerializedName("comf")
+//    public Comfort comfort;
+//
+//    @SerializedName("cw")
+//    public CarWash carwash;
+//
+//    public Suport suport;
+//
+//
+//    private class Comfort {
+//        @SerializedName("txt")
+//        public String info;
+//    }
+//
+//    private class CarWash {
+//        @SerializedName("txt")
+//        public String info;
+//    }
+//
+//    private class Suport {
+//        @SerializedName("txt")
+//        public String info;
+//    }
 }
